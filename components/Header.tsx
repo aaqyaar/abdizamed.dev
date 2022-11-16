2d1751cf7945a82dd260148fbddd574f7176e0fd
abdizamedmo <abdizamedmo@gmail.com>
2022-11-16 23:03:04 +0300

    Header component is Completed and is responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,41 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { HiMenu } from "react-icons/hi";
 
 interface NavType {
   title: string;
   path: string;
   current: boolean;
+}
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="max-w-full py-5 px-5 shadow shadow-green-500 md:px-0 md:shadow-none">
+      <div className="mx-auto flex max-w-[1240px] items-center justify-between">
+        <div id="brand">
+          <h1 className="ml-2 text-2xl font-bold text-green-600">
+            Abdi Zamed Mohamed
+          </h1>
+        </div>
+        <nav id="nav-links" className="flex items-center">
+          {desktopNav()}
+          <div className="ml-4 inline-flex items-center lg:hidden">
+            <button
+              className="rounded-md p-2 text-green-600 hover:bg-green-500 hover:text-green-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-600"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <HiMenu className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+        </nav>
+      </div>
+      {isOpen && mobileNav({ open: isOpen })}
+    </header>
+  );
 }
 
 const RenderNav = ({ title, path, current }: NavType) => {
@@ -14,7 +45,7 @@ const RenderNav = ({ title, path, current }: NavType) => {
         href={path}
         className={`text-md inline-flex px-3 py-2 font-medium ${
           current
-            ? "rounded-none border-b-2 border-green-600 text-green-600"
+            ? "rounded-none border-b-0 border-green-600 text-green-600 md:border-b-2"
             : "text-green-100 hover:text-green-600"
         }`}
       >
@@ -23,47 +54,34 @@ const RenderNav = ({ title, path, current }: NavType) => {
     </li>
   );
 };
-export default function Header() {
-  return (
-    <header className="max-w-full py-5 px-5 shadow md:px-0 md:shadow-none">
-      <div className="mx-auto flex max-w-[1240px] items-center justify-between">
-        <div id="brand">
-          <h1 className="ml-2 text-2xl font-bold text-green-600">
-            Abdi Zamed Mohamed
-          </h1>
-        </div>
-        <nav id="nav-links" className="flex items-center">
-          <ul className="hidden lg:flex lg:items-center">
-            {navigation.map((nav: NavType) => (
-              <RenderNav key={nav.title} {...nav} />
-            ))}
-          </ul>
 
-          <div className="ml-4 inline-flex items-center lg:hidden">
-            <button className="inline-flex items-center justify-center rounded-md p-2 text-green-600 hover:bg-green-100 hover:text-green-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-600">
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
-          </div>
-        </nav>
-      </div>
-    </header>
+const desktopNav = () => {
+  return (
+    <ul className="hidden lg:flex lg:items-center">
+      {navigation.map((nav: NavType) => (
+        <RenderNav key={nav.title} {...nav} />
+      ))}
+    </ul>
   );
-}
+};
+
+const mobileNav = ({ open }: { open: boolean }) => {
+  return (
+    <div className="block md:hidden">
+      <div
+        className={`absolute inset-x-0 ${
+          open && "translate-y-0"
+        } transform p-2 transition duration-300 ease-in-out`}
+      >
+        <ul className="flex flex-col  items-start p-5">
+          {navigation.map((nav: NavType) => (
+            <RenderNav key={nav.title} {...nav} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 const navigation: NavType[] = [
   { title: "Home", path: "#", current: true },
