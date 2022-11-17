2e24bbcdaa9d5f7a459040616bd5464d062744e1
abdizamedmo <abdizamedmo@gmail.com>
2022-11-17 09:53:59 +0300

    Container's setup is ready

---
+++ containers/index.ts
@@ -0,0 +1,4 @@
+export { default as Hero } from "./Hero/Hero";
+export { default as About } from "./About/About";
+export { default as Projects } from "./Projects/Projects";
+export { default as Contact } from "./Contact/Contact";
