c7f81b78e1e127feef765d3509cd592a6d541ef5
abdizamedmo <abdizamedmo@gmail.com>
2022-11-25 16:16:54 +0300

    fixed: blogs lists page error

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,9 +1,7 @@
 import { ComingSoon } from "components";
-import { getBlogs } from "lib/utils";
 
 import React from "react";
 
 export default async function page() {
-  const blogs = await getBlogs();
   return <ComingSoon />;
 }
